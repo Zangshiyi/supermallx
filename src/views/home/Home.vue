330f13bca9c937002bb0c19491a004ed10172601
Zangshiyi <1475391361@qq.com>
2021-07-27 23:08:15 +0800

    day2

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,46 +1,199 @@
 <template>
-    <div id="home">
-      <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-      <home-swiper :banners="banners"></home-swiper>
-      <recommend-view :recommends="recommends"></recommend-view>
-    </div>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>
+    <goods-list :goods="goods['pop'].list"></goods-list>
+
+
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+
 </template>
 
 <script>
-  import NavBar from "../../components/common/navbar/NavBar";
+
   import HomeSwiper from "./childComps/HomeSwiper";
   import RecommendView from "./childComps/RecommendView";
+  import FeatureView from "./childComps/FeatureView";
+
+  import NavBar from "../../components/common/navbar/NavBar";
+  import TabControl from "../../components/content/tabControl/TabControl";
+  import GoodsList from "../../components/content/goods/GoodsList";
+
+  import {getHomeMultidata, getHomeData} from "network/home";
 
 
-  import {getHomeMultidata} from "network/home";
-
-
-    export default {
-        name: "Home",
-        components:{
-          NavBar,
-          HomeSwiper,
-          RecommendView
-        },
-      data(){
-          return {
-            banners:[],
-            recommends: [],
-          }
+  export default {
+    name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+      TabControl,
+      GoodsList
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: [],
+        goods: {
+          'pop': {page: 0, list: []},
+          'new': {page: 0, list: []},
+          'sell': {page: 0, list: []}
+        }
+      }
+    },
+    created() {
+      //1.请求多个数据
+      this.getHomeMultidata()
+      // 2.请求商品数据
+      this.getHomeProducts('pop')
+      this.getHomeProducts('new')
+      this.getHomeProducts('sell')
+    },
+    methods:{
+      getHomeMultidata(){
+        getHomeMultidata().then(res => {
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })
       },
-      created() {
-          //1.请求多个数据
-          getHomeMultidata().then(res=>{
-            this.banners = res.data.banner.list;
-            this.recommends = res.data.recommend.list;
-          })
+      getHomeProducts(type) {
+        getHomeData(type, this.goods[type].page+1).then(res => {
+          const goodsList = res.data.list;
+          this.goods[type].list.push(...goodsList);
+          this.goods[type].page += 1;
+        })
       }
     }
+  }
 </script>
 
 <style scoped>
-.home-nav {
-  background-color: var(--color-tint);
-  color: #ffffff;
-}
+  #home {
+    padding-top: 44px;
+  }
+
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #ffffff;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px;
+    z-index: 9;
+  }
 </style>
